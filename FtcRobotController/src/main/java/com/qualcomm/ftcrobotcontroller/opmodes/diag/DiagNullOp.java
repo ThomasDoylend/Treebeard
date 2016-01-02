b2649c637c8206cffae211758e2604c1748da1fb
ThomasDoylend <tmdoylend@gmail.com>
2016-01-02 15:36:20 -0800

    Refactored NullOp and made it a diag

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/diag/DiagNullOp.java
@@ -0,0 +1,20 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.diag;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by tdoylend on 2016-01-02.
+ */
+public class DiagNullOp extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Diagnostic","Null Operation");
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Diagnostic","Null Operation");
+        telemetry.addData("Doing Nothing","As Desired");
+    }
+}
